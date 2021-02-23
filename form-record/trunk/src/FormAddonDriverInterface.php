d206627983c35276e2deeaf4b800bb2615792f88
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-02-23 10:47:37 +0000

    form-record >> master

---
+++ form-record/trunk/src/FormAddonDriverInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\FormRecord;
+
+use Pollen\Form\AddonDriverInterface;
+
+interface FormAddonDriverInterface extends AddonDriverInterface
+{
+}
---
+++ form-record/trunk/src/RecordMetaModel.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\FormRecord;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use tiFy\Database\MetaModel;
+
+class RecordMetaModel extends MetaModel
+{
+    /**
+     * Nom de qualification de la table associée.
+     * @var string
+     */
+    protected $table = 'tify_forms_recordmeta';
+
+    /**
+     * @return BelongsTo
+     */
+    public function record(): BelongsTo
+    {
+        return $this->belongsTo(RecordModel::class);
+    }
+}
